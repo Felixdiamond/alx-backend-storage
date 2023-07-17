ad48e6b4bd45e7655c981d10ece24294d0d0f6d2
Felixdiamond <diamondfelix006@gmail.com>
2023-07-17 14:31:19 -0700

    mongodb noSQL

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    num_docs = logs_collection.count_documents({})
+    print(f"{num_docs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_method}")
+
+    num_status = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{num_status} status check")
+
+    print("IPs:")
+    top_ips = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")

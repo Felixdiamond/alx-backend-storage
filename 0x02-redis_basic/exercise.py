007e335c11d3824e59674ac0ba61f7c0bb814888
Felixdiamond <diamondfelix006@gmail.com>
2023-07-19 04:07:51 -0700

    redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -61,12 +61,12 @@ def replay(method: Callable):
     """
     input_key = f"{method.__qualname__}:inputs"
     output_key = f"{method.__qualname__}:outputs"
-    
+
     inputs = method.__self__._redis.lrange(input_key, 0, -1)
     outputs = method.__self__._redis.lrange(output_key, 0, -1)
 
     print(f"{method.__qualname__} was called {len(inputs)} times:")
-    
+
     for input_, output in zip(inputs, outputs):
         print(f"{method.__qualname__}(*{input_.decode('utf-8')}) -> "
               f"{output.decode('utf-8')}")
@@ -76,6 +76,7 @@ class Cache:
     """
     Cache class that stores data in a Redis instance.
     """
+
     def __init__(self):
         """
         Initializes a new Cache instance. Creates a new Redis client and
@@ -153,4 +154,3 @@ class Cache:
         """
         value = self._redis.get(key)
         return int.from_bytes(value, byteorder="big")
-

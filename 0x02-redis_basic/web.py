940cacddc2532376c8d137c620677cbc7c89acf4
Felixdiamond <diamondfelix006@gmail.com>
2023-07-19 04:11:19 -0700

    redis basic

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+This module defines a get_page function that obtains the HTML content of a
+particular URL and caches it.
+"""
+import requests
+from typing import Callable
+from functools import wraps
+import redis
+
+redis_client = redis.Redis()
+
+
+def count_url_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts how many times a particular URL is accessed.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        key = f"count:{url}"
+        redis_client.incr(key)
+        return method(url)
+
+    return wrapper
+
+
+@count_url_calls
+def get_page(url: str) -> str:
+    """
+    Obtains the HTML content of a particular URL and caches it with an
+    expiration time of 10 seconds.
+
+    Args:
+        url (str): The URL of the page to be retrieved.
+
+    Returns:
+        str: The HTML content of the page.
+    """
+    cache_key = f"cached:{url}"
+    cached_response = redis_client.get(cache_key)
+
+    if cached_response:
+        return cached_response.decode('utf-8')
+
+    response = requests.get(url)
+    redis_client.setex(cache_key, 10, response.text)
+
+    return response.text
+

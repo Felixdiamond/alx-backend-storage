5e7e05ef59ac672b54db620a61e187a9573b31ff
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-19 21:16:46 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,9 +8,11 @@ import requests
 from functools import wraps
 from typing import Callable
 
-redis_client = redis.Redis()
 
-def count_url_calls(method: Callable) -> Callable:
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
     """
     Decorator that counts how many times a particular URL is accessed.
 
@@ -21,27 +23,21 @@ def count_url_calls(method: Callable) -> Callable:
         Callable: The decorated method.
     """
     @wraps(method)
-    def wrapper(url: str) -> str:
-        redis_client.incr(f'count:{url}')
-        result = redis_client.get(f'result:{url}')
+    def wrapper(url) -> str:
+        '''The wrapper function for caching the output.'''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_client.set(f'count:{url}', 0)
-        redis_client.setex(f'result:{url}', 10, result)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
     return wrapper
 
-@count_url_calls
+
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Obtains the HTML content of a particular URL and caches it with an
-    expiration time of 10 seconds.
-
-    Args:
-        url (str): The URL of the page to be retrieved.
-
-    Returns:
-        str: The HTML content of the page.
-    """
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request'''
     return requests.get(url).text

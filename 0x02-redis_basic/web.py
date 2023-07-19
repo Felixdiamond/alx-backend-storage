233c5146c482479f8a2b24c69a59a5a20a0c5756
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-19 21:12:20 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,14 +4,12 @@ This module defines a get_page function that obtains the HTML content of a
 particular URL and caches it.
 """
 import requests
-from typing import Callable
 from functools import wraps
 import redis
 
 redis_client = redis.Redis()
 
-
-def count_url_calls(method: Callable) -> Callable:
+def count_url_calls(method):
     """
     Decorator that counts how many times a particular URL is accessed.
 
@@ -22,43 +20,14 @@ def count_url_calls(method: Callable) -> Callable:
         Callable: The decorated method.
     """
     @wraps(method)
-    def wrapper(url: str) -> str:
+    def wrapper(url):
         key = f"count:{url}"
         redis_client.incr(key)
         return method(url)
-
     return wrapper
-
-
-def cache_result(method: Callable) -> Callable:
-    """
-    Decorator that caches the result of a method using Redis.
-
-    Args:
-        method (Callable): The method to be decorated.
-
-    Returns:
-        Callable: The decorated method.
-    """
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        cache_key = f"cached:{url}"
-        cached_response = redis_client.get(cache_key)
-
-        if cached_response:
-            return cached_response.decode('utf-8')
-
-        response = method(url)
-        redis_client.setex(cache_key, 10, response)
-
-        return response
-
-    return wrapper
-
 
 @count_url_calls
-@cache_result
-def get_page(url: str) -> str:
+def get_page(url):
     """
     Obtains the HTML content of a particular URL and caches it with an
     expiration time of 10 seconds.
@@ -69,6 +38,13 @@ def get_page(url: str) -> str:
     Returns:
         str: The HTML content of the page.
     """
+    cache_key = f"cached:{url}"
+    cached_response = redis_client.get(cache_key)
+
+    if cached_response:
+        return cached_response.decode('utf-8')
+
     response = requests.get(url)
+    redis_client.setex(cache_key, 10, response.text)
+
     return response.text
-    

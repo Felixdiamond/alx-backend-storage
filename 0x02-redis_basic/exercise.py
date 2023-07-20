caf10bb37e9ace88b268ab0ea14c92a4f6fcded6
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-20 01:28:51 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis, Reading from Redis and recovering original type,
     Incrementing values, Storing lists, Retrieving lists """
-from typing import Union, Callable, Optional, Any
+from typing import Union, Callable, Any, Optional
 import redis
 import uuid
 from functools import wraps
 
 
 def call_history(method: Callable) -> Callable:
-    """
-    Decorator that stores the history of inputs and outputs for a particular
-    function.
-
-    Args:
-        method (Callable): The method to be decorated.
-
-    Returns:
-        Callable: The decorated method.
-    """
+    """ store the history of inputs and outputs for a particular function """
     key = method.__qualname__
     inputs = key + ":inputs"
     outputs = key + ":outputs"
@@ -33,15 +24,7 @@ def call_history(method: Callable) -> Callable:
 
 
 def count_calls(method: Callable) -> Callable:
-        """
-    Decorator that counts how many times a method of the Cache class is called.
-
-    Args:
-        method (Callable): The method to be decorated.
-
-    Returns:
-        Callable: The decorated method.
-    """
+    """ to count how many times methods of the Cache class are called """
     key = method.__qualname__
 
     @wraps(method)
@@ -53,80 +36,39 @@ def count_calls(method: Callable) -> Callable:
 
 
 class Cache:
-    """
-    Cache class that stores data in a Redis instance.
-    """
+    """ class """
     def __init__(self):
-        """
-        Initializes a new Cache instance. Creates a new Redis client and
-        flushes the instance using flushdb.
-        """
+        """ constructor - store an instance of the Redis client as a private
+        variable named _redis and flush the instance using flushdb """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Stores the input data in the Redis instance using a randomly generated
-        key and returns the key.
-
-        Args:
-            data (Union[str, bytes, int, float]): The data to be stored.
-
-        Returns:
-            str: The key used to store the data.
-        """
+        """ generate a random key (e.g. using uuid), store the input data in
+        Redis using the random key and return the key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """
-        Retrieves the data stored at the specified key in the Redis instance
-        and returns it. If a callable is provided as the fn argument, it is
-        used to convert the data back to the desired format.
-
-        Args:
-            key (str): The key of the data to be retrieved.
-            fn (Optional[Callable]): A callable used to convert the data back
-                to the desired format.
-
-        Returns:
-            Union[str, bytes, int, float]: The data stored at the specified key
-        """
+        """ take a key string argument and an optional Callable argument named
+            fn. This callable will be used to convert the data back to the
+            desired format """
         data = self._redis.get(key)
         if fn:
             return fn(data)
         return data
 
     def get_str(self, key: str) -> str:
-        """
-        Retrieves the data stored at the specified key in the Redis instance,
-        converts it to a string using decode("utf-8"), and returns it.
-
-        Args:
-            key (str): The key of the data to be retrieved.
-
-        Returns:
-            str: The data stored at the specified key as a string.
-        """
+        """ automatically parametrize Cache.get to str """
         data = self._redis.get(key)
         return data.decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        """
-        Retrieves the data stored at the specified key in the Redis instance,
-        converts it to an integer using int.from_bytes(value, byteorder), and
-        returns it.
-
-        Args:
-            key (str): The key of the data to be retrieved.
-
-        Returns:
-            int: The data stored at the specified key as an integer.
-        """
+        """ automatically parametrize Cache.get to int """
         data = self._redis.get(key)
         try:
             data = int(value.decode("utf-8"))
@@ -136,12 +78,7 @@ class Cache:
 
 
 def replay(method: Callable):
-    """
-    Displays the history of calls of a particular function.
-
-    Args:
-        method (Callable): The method whose call history is to be displayed.
-    """
+    """ display the history of calls of a particular function """
     key = method.__qualname__
     inputs = key + ":inputs"
     outputs = key + ":outputs"

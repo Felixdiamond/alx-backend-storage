6ee416fe8a70df011ddcd650fe8c141fa7d49f2b
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-07-20 01:40:12 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,40 +3,34 @@
 This module defines a get_page function that obtains the HTML content of a
 particular URL and caches it.
 """
-import requests
-from typing import Callable
-from functools import wraps
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
-redis_client = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.'''
 
 
-def count_url_calls(method: Callable) -> Callable:
-    """
-    Decorator that counts how many times a particular URL is accessed.
-    """
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.'''
     @wraps(method)
-    def wrapper(url: str) -> str:
-        key = f"count:{url}"
-        redis_client.incr(key)
-        return method(url)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.'''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-    return wrapper
 
-
-@count_url_calls
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    Obtains the HTML content of a particular URL and caches it with an
-    expiration time of 10 seconds.
-    """
-    cache_key = f"cached:{url}"
-    cached_response = redis_client.get(cache_key)
-
-    if cached_response:
-        return cached_response.decode('utf-8')
-
-    response = requests.get(url)
-    redis_client.setex(cache_key, 10, response.text)
-
-    return response.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request'''
+    return requests.get(url).text
